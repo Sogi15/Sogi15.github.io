3f0c6ee9d98df4306e087aff6447ebc405b3d1c7
Sogi15 <90307491+Sogi15@users.noreply.github.com>
2022-03-25 21:54:56 +0100

    updated

---
+++ res/js/content.js
@@ -0,0 +1,12 @@
+function rolam() {
+    document.getElementById("content").innerHTML='<object type="text/html" data="res/rolam.html" style="width:100%;height:25vw;"></object>';
+    document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
+}
+function menu() {
+    document.getElementById("content").innerHTML='<object type="text/html" data="res/menu.html" style="width:100%;height:25vw;"></object>';
+    document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
+}
+function gepem() {
+    document.getElementById("content").innerHTML='<object type="text/html" data="res/gepem.html" style="width:100%;height:25vw;"></object>';
+    document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
+}
