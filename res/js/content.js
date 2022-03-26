a100ce9909b88370b440252d4a6f1fa0d4ef3488
Sogi15 <90307491+Sogi15@users.noreply.github.com>
2022-03-26 22:14:45 +0100

    new features added

--- res/js/content.js
+++ res/js/content.js
@@ -1,3 +1,6 @@
+document.getElementById("rogstrix").onmouseover = function() {document.getElementById("rogstriximg").style.display="block";document.getElementById("rogstriximgshowoff").style.display = "none";};
+document.getElementById("rogstrix").onmouseout = function() {document.getElementById("rogstriximg").style.display="none";document.getElementById("rogstriximgshowoff").style.display = "block";};
+
 function rolam() {
     document.getElementById("content").innerHTML='<object type="text/html" data="res/rolam.html" style="width:100%;height:25vw;"></object>';
     document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
@@ -7,7 +10,7 @@ function menu() {
     document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
 }
 function gepem() {
-    document.getElementById("content").innerHTML='<object type="text/html" data="res/gepem.html" style="width:100%;height:30vw;"></object>';
+    document.getElementById("content").innerHTML='<object type="text/html" data="res/gepem.html" style="width:100%;height:35vw;"></object>';
     document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
 }
 
