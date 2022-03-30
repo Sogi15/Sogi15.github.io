c8bc066c8462533270dac4031cd62b3d87d176fc
Sogi15 <90307491+Sogi15@users.noreply.github.com>
2022-03-30 18:56:58 +0200

    Mobile menu new features and menu rólam have new update

--- res/js/content.js
+++ res/js/content.js
@@ -1,15 +1,22 @@
 // menü onclick
+
 function rolam() {
     document.getElementById("content").innerHTML='<object type="text/html" data="res/rolam.html" style="width:100%;height:25vw;"></object>';
     document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
+    document.getElementById("menu_rolam").style.color = "orange";
+    document.getElementById("menu_rolam").style.background = "linear-gradient(black 80%, red)";
 }
 function menu() {
     document.getElementById("content").innerHTML='<object type="text/html" data="res/menu.html" style="width:100%;height:25vw;"></object>';
     document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
+    document.getElementById("menu_rolam").style.color = "white";
+    document.getElementById("menu_rolam").style.background = "black";
 }
 function gepem() {
     document.getElementById("content").innerHTML='<object type="text/html" data="res/gepem.html" style="width:100%;height:50vw;"></object>';
     document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
+    document.getElementById("menu_rolam").style.color = "white";
+    document.getElementById("menu_rolam").style.background = "black";
 }
 
 // On Mobile
@@ -17,11 +24,22 @@ let mobilmenumegjelenit = 1; // változó, hogy ha mobilon a menüre kattintunk 
 function gepemM() {
     document.getElementById("content").innerHTML='<object type="text/html" data="res/gepem.html" style="width:100%;height:140vw;"></object>';
     document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
+    document.getElementById("menu_gepem").style.color = "orange";
+    document.getElementById("menu_rolamM").style.color = "white";
 }
 function rolamM() {
     document.getElementById("content").innerHTML='<object type="text/html" data="res/rolam.html" style="width:100%;height:140vw;"></object>';
     document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
+    document.getElementById("menu_gepem").style.color = "white";
+    document.getElementById("menu_rolamM").style.color = "orange";
 }
 function mobilmenu(){
-    if (mobilmenumegjelenit==1) {document.getElementById("mobilmenu").style.display = "block";
-    mobilmenumegjelenit = 0;}else {document.getElementById("mobilmenu").style.display = "none";mobilmenumegjelenit=1;}}
+    if (screen.width <= 768)
+    {
+    if (mobilmenumegjelenit==1) {
+        document.getElementById("mobilmenu").style.display = "block";mobilmenumegjelenit = 0;
+        document.getElementById("mobilmenuclick").style.color = "orange";}
+        else {document.getElementById("mobilmenu").style.display = "none";mobilmenumegjelenit=1;document.getElementById("mobilmenuclick").style.color = "white";}
+    }
+    else {document.getElementById("mobilmenu").style.display = "none";}
+}
