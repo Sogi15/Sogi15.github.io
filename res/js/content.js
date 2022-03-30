e3bdbe65e6914461414b5cc9ba3b288648b41863
Sogi15 <90307491+Sogi15@users.noreply.github.com>
2022-03-30 20:04:01 +0200

    h1 new animated bg and stroke

--- res/js/content.js
+++ res/js/content.js
@@ -1,22 +1,25 @@
 // menü onclick
-
 function rolam() {
     document.getElementById("content").innerHTML='<object type="text/html" data="res/rolam.html" style="width:100%;height:25vw;"></object>';
     document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
     document.getElementById("menu_rolam").style.color = "orange";
     document.getElementById("menu_rolam").style.background = "linear-gradient(black 80%, red)";
+    document.getElementById("menu_test").style.color = "";
+    document.getElementById("menu_test").style.background = "";
 }
 function menu() {
     document.getElementById("content").innerHTML='<object type="text/html" data="res/menu.html" style="width:100%;height:25vw;"></object>';
     document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
-    document.getElementById("menu_rolam").style.color = "white";
-    document.getElementById("menu_rolam").style.background = "black";
+    document.getElementById("menu_test").style.color = "orange";
+    document.getElementById("menu_test").style.background = "linear-gradient(black 80%, red)";
+    document.getElementById("menu_rolam").style.color = "";document.getElementById("menu_rolam").style.background = "";
 }
 function gepem() {
     document.getElementById("content").innerHTML='<object type="text/html" data="res/gepem.html" style="width:100%;height:50vw;"></object>';
     document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
-    document.getElementById("menu_rolam").style.color = "white";
-    document.getElementById("menu_rolam").style.background = "black";
+    document.getElementById("menu_rolam").style.color = "";document.getElementById("menu_rolam").style.background = "";
+    document.getElementById("menu_test").style.color = "";
+    document.getElementById("menu_test").style.background = "";
 }
 
 // On Mobile
@@ -25,12 +28,12 @@ function gepemM() {
     document.getElementById("content").innerHTML='<object type="text/html" data="res/gepem.html" style="width:100%;height:140vw;"></object>';
     document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
     document.getElementById("menu_gepem").style.color = "orange";
-    document.getElementById("menu_rolamM").style.color = "white";
+    document.getElementById("menu_rolamM").style.color = "";
 }
 function rolamM() {
     document.getElementById("content").innerHTML='<object type="text/html" data="res/rolam.html" style="width:100%;height:140vw;"></object>';
     document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
-    document.getElementById("menu_gepem").style.color = "white";
+    document.getElementById("menu_gepem").style.color = "";
     document.getElementById("menu_rolamM").style.color = "orange";
 }
 function mobilmenu(){
