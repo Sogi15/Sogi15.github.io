fd89c75c120cc8b70ac3d7f4b5fa18352bd6e7c1
Sogi15 <90307491+Sogi15@users.noreply.github.com>
2022-03-30 12:56:05 +0200

    mobil menu testing and feturing

--- res/js/content.js
+++ res/js/content.js
@@ -12,7 +12,22 @@ function gepem() {
 }
 
 // On Mobile
+let mobilmenumegjelenit = 1; // változó, hogy ha mobilon a menüre kattintunk megjelenjen majd eltünjön
 function gepemM() {
     document.getElementById("content").innerHTML='<object type="text/html" data="res/gepem.html" style="width:100%;height:140vw;"></object>';
     document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
+}
+function rolamM() {
+    document.getElementById("content").innerHTML='<object type="text/html" data="res/rolam.html" style="width:100%;height:140vw;"></object>';
+    document.getElementById("home").innerHTML="<style>span#home {display:none;}</style>";
+}
+function mobilmenu(){
+    if (mobilmenumegjelenit==1) {
+    document.getElementById("mobilmenu").style.display = "block";
+    mobilmenumegjelenit = 0;
+    }
+    else {
+        document.getElementById("mobilmenu").style.display = "none";
+        mobilmenumegjelenit=1;
+    }
 }
